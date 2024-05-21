648a824b35bc2318d1032a6ba4820fc03abeb681
Almaz Orymbetov <230585@astanait.edu.kz>
2024-05-21 16:20:22 +0500

    Vertex

--- src/Vertex.java
+++ src/Vertex.java
@@ -1,6 +1,12 @@
+import java.util.HashMap;
+import java.util.Map;
 public class Vertex<V>{
     private V data;
     private Map<Vertex<V>,Double> adjacentVertices;
+    public Vertex(V data){
+        this.data=data;
+        this.adjacentVertices=new HashMap<>();
+    }
     public void addAdjacentVertex(Vertex<V> destination,Double weight){
         adjacentVertices.put(destination,weight);
     }
